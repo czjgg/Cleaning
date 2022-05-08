db68ac90d34c6b6ca682cc5cf116a2cbd8055912
lab <2236872724@qq.com>
2022-05-08 11:12:10 +0800

    二叉树的所有路径

--- ws/tree/tree.cpp
+++ ws/tree/tree.cpp
@@ -39,9 +39,39 @@ void treeInsert(TreeNode* cur,int val,bool left){
   return;
 }
 class Solution {
+private:
+
+    void traversal(TreeNode* cur, vector<int>& path, vector<string>& result) {
+      path.push_back(cur->val);
+      if(cur->left==nullptr&&cur->right==nullptr){
+        string sPath;
+        for(int i=0;i<path.size()-1;i++){
+          sPath += to_string(path[i]);
+          sPath += "->";
+        }
+        sPath += to_string(path[path.size()-1]);
+        result.push_back(sPath);
+        return ;
+      }
+      if(cur->left){
+        traversal(cur->left,path,result);
+        path.pop_back();
+      }
+      if(cur->right){
+        traversal(cur->right,path,result);
+        path.pop_back();
+      }
+    }
 public:
     int result;
 
+    vector<string> binaryTreePaths(TreeNode* root) {
+      vector<string> result;
+      vector<int> path;
+      if(root==nullptr) return result;
+      traversal(root,path,result);
+      return result;
+    }
     int getHeight(TreeNode* node) {
       if(node ==nullptr){
         return 0;
