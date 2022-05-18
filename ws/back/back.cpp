631b795d07a20f09798c2b51f34ce8faa13d52b6
lab <2236872724@qq.com>
2022-05-18 14:17:12 +0800

    93.复原IP地址

--- ws/back/back.cpp
+++ ws/back/back.cpp
@@ -30,6 +30,44 @@ private:
         "wxyz", // 9
     };
 
+    void backtrackingToCheckIPAddress(string& s, int startIndex, int pointNum){
+      if(pointNum == 3){
+        if(isValidIPAddress(s, startIndex, s.size()-1)){
+          resultStr.push_back(s);
+        }
+        return;
+      }
+      for(int i = startIndex; i < s.size(); i++){
+        if(isValidIPAddress(s, startIndex, i)){
+          s.insert(s.begin() + i + 1, '.');
+          pointNum++;
+          backtrackingToCheckIPAddress(s, i + 2, pointNum);
+          pointNum--;
+          s.erase(s.begin()+ i + 1);
+        }else break;
+      }
+      
+    }
+    bool isValidIPAddress(const string& s, int start, int end){
+      if(start > end){
+        return false;
+      }
+      if(s[start] == '0' && start != end){
+        return false;
+      }
+      int num = 0;
+      for(int i = start; i <= end; i++){
+        if(s[i] > '9' || s[i] < '0'){
+          return false;
+        }else{
+          num = 10 * num + (s[i] - '0');
+        }
+        if(num > 255){
+        return  false;
+        }
+      }
+      return true;
+    }
     void backtrackingToDividePalindrome (const string& s, int startIndex) {
       if(startIndex>=s.size()){
         resultPalindrome.push_back(pathPalindrome);
@@ -129,6 +167,12 @@ private:
       }
     }
 public:
+    vector<string> restoreIpAddresses(string s) {
+      resultStr.clear();
+      if(s.size() > 12) return resultStr;
+      backtrackingToCheckIPAddress(s, 0, 0);
+      return resultStr;
+    }
     vector<vector<string>> partition(string s) {
       resultPalindrome.clear();
       pathPalindrome.clear();
