cf09cf326741aed26da184baa4ad8a02ee227a05
lab <2236872724@qq.com>
2022-06-02 14:22:52 +0800

    零钱兑换

--- ws/DP/dynamic programming.cpp
+++ ws/DP/dynamic programming.cpp
@@ -8,7 +8,31 @@
 using namespace std;
 class Solution {
 public:
-
+  int coinChange(vector<int>& coins, int amount) {
+    if(amount == 0) return 0;
+    vector<int> dp(amount + 1,INT_MAX);
+    dp[0] = 0;
+    for(int i = 1; i <= amount; i++){
+      for(int j = 0; j < coins.size(); j++){
+        if(i >= coins[j]){
+          dp[i] = min(dp[i],dp[i - coins[j]] + 1);
+        }
+      }
+    }
+    return dp[amount];
+  }
+  int combinationSum4(vector<int>& nums, int target) {
+    vector<int> dp(target + 1, 0);
+    dp[0] = 1;
+    for(int j = 0; j <= target; j++){
+      for(int i = 0; i < nums.size(); i++){
+        if(j - nums[i] >= 0 && dp[i] < INT_MAX - dp[j - nums[i]]){
+          dp[j] += dp[j - nums[i]];
+        }
+      }
+    }
+    return dp[target];
+  }
   int change(int amount, vector<int>& coins) {
     vector<int> dp(amount + 1, 0);
     dp[0] = 1;
