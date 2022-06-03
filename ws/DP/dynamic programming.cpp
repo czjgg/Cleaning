5c1a257da8ea258a493444078a2fa28e68f97f84
lab <2236872724@qq.com>
2022-06-03 14:09:06 +0800

    动态规划.完全平方数

--- ws/DP/dynamic programming.cpp
+++ ws/DP/dynamic programming.cpp
@@ -8,6 +8,16 @@
 using namespace std;
 class Solution {
 public:
+  int numSquares(int n) {
+    vector<int> dp(n + 1, INT_MAX);
+    dp[0] = 0;
+    for(int i = 1; i*i < n;i++){
+      for(int j = i*i; j <= n; j++){
+        dp[j] = min(dp[j], dp[j - i*i] + 1);
+      }
+    }
+    return dp[n];
+  }
   int coinChange(vector<int>& coins, int amount) {
     if(amount == 0) return 0;
     vector<int> dp(amount + 1,INT_MAX);
