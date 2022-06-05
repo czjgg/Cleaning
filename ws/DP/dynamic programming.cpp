0896460a8072799e9b63136550ffab881bbb76e6
lab <2236872724@qq.com>
2022-06-05 11:13:03 +0800

    打家劫舍

--- ws/DP/dynamic programming.cpp
+++ ws/DP/dynamic programming.cpp
@@ -6,9 +6,31 @@
 #include<math.h>
 #include<algorithm>
 using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
 class Solution {
 public:
+  int rob3(TreeNode* root) {
+    vector<int> result = robTree(root);
+    return max(result[0],result[1]);
+  }
+  vector<int> robTree(TreeNode* cur){
+    if(cur == nullptr) return vector<int>{0,0};
+    vector<int> left = robTree(cur->left);
+    vector<int> right = robTree(cur->right);
 
+    int val1 = cur->val + left[0] + right[0];
+    int val2 = max(left[0], left[1]) + max(right[0], right[1]);
+
+    return {val2,val1};
+  }
   int rob2(vector<int>& nums) {
     if(nums.size() == 0) return 0;
     if(nums.size() == 1) return nums[0];
