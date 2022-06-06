61a3f9da6390b41c576313c70c9ca22eac77595c
lab <2236872724@qq.com>
2022-06-06 21:10:06 +0800

    股票4

--- ws/DP/dynamic programming.cpp
+++ ws/DP/dynamic programming.cpp
@@ -17,6 +17,71 @@ struct TreeNode {
 };
 class Solution {
 public:
+  int maxProfit(vector<int>& prices) {
+    //greedy
+    // int low = INT_MAX;
+    // int result = 0;
+    // for(int i = 0; i < prices.size(); i++){
+    //   low = min(low, prices[i]);
+    //   result = max(result, prices[i] - low);
+    // }
+    // return result;
+
+    //dp
+    // int len = prices.size();
+    // if(len == 0 ) return 0;
+    // vector<vector<int>> dp(2,vector<int>(2));
+    // dp[0][0] -= prices[0];
+    // dp[0][1] = 0;
+    // for(int i = 1;i < len; i++){
+    //   dp[i%2][0] = max(dp[(i - 1) % 2][0], -prices[i]);
+    //   dp[i%2][1] = max(dp[(i - 1) % 2][1], dp[(i - 1) % 2][0] + prices[i]);
+    // }
+    // return dp[(len - 1) % 2][1];
+
+    //dp 可以买卖多次
+    // int len = prices.size();
+    // if(len == 0 ) return 0;
+    // vector<vector<int>> dp(2,vector<int>(2));
+    // dp[0][0] -= prices[0];
+    // dp[0][1] = 0;
+    // for(int i = 1;i < len; i++){
+    //   dp[i%2][0] = max(dp[(i - 1) % 2][0], dp[(i - 1) % 2][1] - prices[i]);
+    //   dp[i%2][1] = max(dp[(i - 1) % 2][1], dp[(i - 1) % 2][0] + prices[i]);
+    // }
+    // return dp[(len - 1) % 2][1];
+  
+    //dp 只能买两次
+    // if(prices.size() == 0) return 0;
+    // vector<vector<int>> dp(prices.size(), vector<int>(5, 0));
+    // dp[0][1] = -prices[0];
+    // dp[0][3] = -prices[0];
+    // for(int i = 1; i < prices.size(); i++){
+    //   dp[i][1] = max(dp[i - 1][0] - prices[i], dp[i - 1][1]);
+    //   dp[i][2] = max(dp[i - 1][1] + prices[i], dp[i - 1][2]);
+    //   dp[i][3] = max(dp[i - 1][2] - prices[i], dp[i - 1][3]);
+    //   dp[i][4] = max(dp[i + 1][3] + prices[i], dp[i - 1][4]);
+    // }
+    // return dp[prices.size() - 1][4];
+
+    
+  }
+
+  //可以买k次
+  int maxProfit(int k, vector<int>& prices) {
+    if(prices.size() == 0) return 0;
+    vector<vector<int>> dp(prices.size(), vector<int>(2 * k + 1, 0));
+    for(int i = 0;i <= k; i++){
+      dp[0][2 * i + 1] = -prices[0];
+    }
+    for(int i = 1; i < prices.size(); i++){
+      for(int j = 0; j < k; j++){
+        dp[i][2 * j + 1] = max(dp[i - 1][2 * j] - prices[i], dp[i - 1][2 * j + 1]);
+        dp[i][2 * j + 2] = max(dp[i - 1][2 * j + 1] + prices[i], dp[i - 1][2 * j + 2]);
+      }
+    }
+    return dp[prices.size() - 1][2 * k];
+  }
   int rob3(TreeNode* root) {
     vector<int> result = robTree(root);
     return max(result[0],result[1]);
