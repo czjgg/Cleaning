a90bd550e9fc328a6eeac12221e5fbeccec300b6
lab <2236872724@qq.com>
2022-06-16 14:58:42 +0800

    编辑距离结束

--- ws/DP/dynamic programming.cpp
+++ ws/DP/dynamic programming.cpp
@@ -17,6 +17,44 @@ struct TreeNode {
 };
 class Solution {
 public:
+  int minDistance(string word1, string word2) {
+    vector<vector<int>> dp(word1.size() + 1,vector<int>(word2.size() + 1));
+    for(int i = 0; i <= word1.size(); i++) {
+      dp[i][0] = i;
+    }
+    for(int j = 1; j <= word2.size(); j++){
+      dp[0][j] = j;
+    }
+    for(int i = 1; i <= word1.size(); i++){
+      for(int j = 1; j <= word2.size(); j++){
+        if(word1[i - 1] == word2[j - 1]){
+          dp[i][j] = dp[i - 1][j - 1];
+        }else{
+          dp[i][j] = min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]) + 1;
+        }
+      }
+    }
+    return dp[word1.size()][word2.size()];
+  }
+  int minDistance(string word1, string word2) {
+    vector<vector<int>> dp(word1.size() + 1, vector<int>(word2.size() + 1));
+    for(int i = 0; i <= word1.size(); i++){
+      dp[i][0] = i;
+    }
+    for(int j = 1; j <= word2.size(); j++){
+      dp[0][j] = j;    
+    }
+    for(int i = 1; i <= word1.size(); i++){
+      for( int j = 1; j <= word2.size(); j++){
+        if(word1[i] == word2[j]){
+          dp[i][j] = dp[i - 1][j - 1];
+        }else{
+          dp[i][j] = min(min(dp[i - 1][j] + 1, dp[i][j - 1] + 1), dp[i - 1][j - 1] + 2);
+        }
+      }
+    }
+    return dp[word1.size()][word2.size()];
+  }
   int numDistinct(string s, string t) {
     vector<vector<uint64_t>> dp(s.size() + 1,vector<uint64_t>(t.size() + 1));
     for(int i = 0; i <= s.size(); i++) dp[i][0] = 1;
