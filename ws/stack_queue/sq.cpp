6ced6323e439d01da362357bb5ec26d3290b6b4a
lab <2236872724@qq.com>
2022-06-20 10:36:58 +0800

    单调栈

--- ws/stack_queue/sq.cpp
+++ ws/stack_queue/sq.cpp
@@ -72,6 +72,18 @@ public:
 class deQueue {
   public:
     deque<int> que;
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+      stack<int> st;
+      vector<int> result(temperatures.size(),0);
+      for(int i = 0;i < temperatures.size(); i++){
+        while(!st.empty() && temperatures[i] > temperatures[st.top()]){
+          result[st.top()] = i - st.top();
+          st.pop();
+        }
+        st.push(i);
+      }
+      return result;
+    }
     void pop(int value){
       if(!que.empty()&&value==que.front()){
         que.pop_front();
