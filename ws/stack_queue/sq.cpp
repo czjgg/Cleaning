c481516e8f16280679eb03c188f911708b6ebd52
lab <2236872724@qq.com>
2022-06-23 15:28:56 +0800

    接水

--- ws/stack_queue/sq.cpp
+++ ws/stack_queue/sq.cpp
@@ -72,6 +72,33 @@ public:
 class deQueue {
   public:
     deque<int> que;
+    int trap(vector<int>& height) {
+      if(height.size() <= 2) return 0;
+      stack<int> st;
+      st.push(0);
+      int sum = 0;
+      for(int i = 1; i < height.size(); i++){
+        if(height[i] < height[st.top()]){
+          st.push(i);
+        }
+        if(height[i] == height[st.top()]){
+          st.pop();
+          st.push(i);
+        }else{
+          while(!st.empty() && height[i] > height[st.top()]){
+            int mid = height[st.top()];
+            st.pop();
+            if(!st.empty()){
+              int h = min(height[st.top()], height[i]) - mid;
+              int w = i - st.top() - 1;
+              sum += w * h;
+            }
+          }
+          st.push(i);
+        }
+      }
+      return sum;
+    }
     vector<int> nextGreaterElements(vector<int>& nums) {
       stack<int> st;
       vector<int> result(nums.size(), - 1);
